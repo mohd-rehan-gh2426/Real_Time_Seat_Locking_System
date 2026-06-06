dedd2ba76f0f84a0daf41d61862d9f4cd3d4622c
Mohd Rehan <mdrehanchhipa@gmail.com>
2026-06-06 21:53:15 +0530

    Implement Profile page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/shared/Footer";
 import Home from "./pages/Home.jsx";
 import Movies from "./pages/Movies.jsx";
 import MovieDetails from "./pages/MovieDetails.jsx";
+import Profile from "./pages/Profile.jsx";
 
 
 
@@ -19,7 +20,8 @@ function App() {
             <Route path="/" element={<Home/>}/>
             <Route path="/profile/:id" element={<h1>Profile Page</h1>}/>
             <Route path="/movies" element={<Movies/>}/>
-             <Route path="/movies/:movieId" element={<MovieDetails />} />
+            <Route path="/movies/:movieId" element={<MovieDetails />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
           
         </main>
