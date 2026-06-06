91f502a2035fa882017d43291c50674f3eb17ae6
Mohd Rehan <mdrehanchhipa@gmail.com>
2026-06-06 15:37:44 +0530

    make the recommended movie component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import BannerSlider from '../components/shared/BannerSlider.jsx'
+import Recommended from '../components/Recommended.jsx'
 
 const Home = () => {
   return (
     <div>
         <BannerSlider />
-       
+        <Recommended/>
     </div>
   )
 }
