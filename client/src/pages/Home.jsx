ea90fe7a0e086c005dba045ea68c05152a2a7140
Mohd Rehan <mdrehanchhipa@gmail.com>
2026-06-06 15:41:45 +0530

    implemented list of all events

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import BannerSlider from '../components/shared/BannerSlider.jsx'
 import Recommended from '../components/Recommended.jsx'
+import LiveEvents from '../components/LiveEvents.jsx'
 
 const Home = () => {
   return (
     <div>
         <BannerSlider />
         <Recommended/>
+        <LiveEvents/>
     </div>
   )
 }
